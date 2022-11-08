6dc8d41341f5053e0b2525756d25a988354ca285
Douglas <dailibras@gmail.com>
2022-11-08 16:23:57 -0300

    first commit

---
+++ public/script.js
@@ -0,0 +1,102 @@
+
+ var message = document.getElementById('input_message')
+ const socket = io.connect();
+ const content = document.getElementById('content')
+ const btn = document.getElementById('send')
+ const currentdate = new Date();
+ var userId
+ const chat = {
+     users:{}
+ }
+ socket.on('connect', setUserId)
+ socket.on('sendMessage', sendMessage)
+ socket.on('currentMessages', currentState)
+
+ function currentState(chat){
+     for(messages in chat.messages){
+        const time = chat.messages[messages].time
+         const id = chat.messages[messages].user_id
+         const row = chat.messages[messages].row
+         const message = chat.messages[messages].user_message
+         const color = chat.messages[messages].color
+         sendMessage(message, id, color, row, time)
+     }
+ }
+
+ function setUserId(){
+     userId = socket.id
+     chat.users[userId] = userId
+     console.log(chat.users)
+ }
+
+ btn.addEventListener('touchend', e=>{
+    const time = currentdate.getHours() + ":" + currentdate.getMinutes()
+     if(message.value !== ''){
+        const row = null
+        const user_color = null
+         sendMessage(message.value, userId, user_color, row, time)
+         socket.emit('message', message.value, userId)
+         message.value = "";
+     }
+     e.preventDefault()
+ })
+
+ btn.addEventListener('click', e=>{
+    const time = currentdate.getHours() + ":" + currentdate.getMinutes()
+     if(message.value !== ''){
+        const row = null
+        const user_color = null
+         sendMessage(message.value, userId, user_color, row, time)
+         socket.emit('message', message.value, userId)
+         message.value = "";
+     }
+     e.preventDefault()
+     
+ })
+ function enter(){
+    const time = currentdate.getHours() + ":" + currentdate.getMinutes()
+     if(message.value !== ''){
+        const row = null
+        const user_color = null
+         sendMessage(message.value, userId, user_color, row, time)
+         socket.emit('message', message.value, userId)
+         message.value = "";
+     }
+ }
+
+ function sendMessage(message, user, color, row, time){
+    append_message = document.createElement('div')
+    append_message.classList = 'in'
+    console.log(time)
+    if(user !== userId){
+        if(!row){
+            append_message.style = "margin-top:10px;"
+            append_message.innerHTML = `<span class='message'>
+            <a class='name' style='color:${color};'>${user}</a><br>
+            <a>${message}</a>
+           
+                <a class='time'>${time}</a>
+       
+            </span>`
+        }else{
+            append_message.innerHTML = `<span class='message'>
+            <a>${message}</a>
+            
+                <a class='time'>${time}</a>
+           
+           
+        </span>`
+        }
+    }else{
+        append_message.classList = 'out'
+        append_message.innerHTML = `<span class='message user'>
+        <a>${message}</a>
+       
+                <a class='time'>${time}</a>
+         
+        </span>`
+    }
+ 
+    content.append(append_message)
+    append_message.scrollIntoView();
+ }
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000
+const server = app.listen(port);
+app.use(express.static('public'));
+const socket = require('socket.io');
+const io = socket(server);
+const currentdate = new Date();
+const chat = {
+    messages:[],
+    color:{}
+}
+io.sockets.on('connection', (socket)=>{
+    color = ['#d88deb', '#17c29c', '#fa6533', '#23d366', '#42c7b8', '#a5b337', '#fc9775', '#48a6fd', '#53bdeb', '#e26ab6','#fc9760', '#e26ab6', '#a791ff', '#ffbc38', '#f15c6d']
+    const color_range = Math.floor(Math.random() * color.length);
+    console.log('conectado')
+
+    const userId = socket.id;
+    chat.color[userId] = color[color_range];
+    console.log(chat.color)
+    socket.emit('currentMessages', chat)
+    socket.on('message', sendMessage)
+    function sendMessage(message, id){
+        const time = currentdate.getHours() + ":" + currentdate.getMinutes()
+        const lastMessage = chat.messages[chat.messages.length - 1]
+        if(lastMessage){
+            console.log(lastMessage)
+            if(lastMessage.user_id == id){
+                chat.messages.push({user_id:id, user_message:message, color:chat.color[id], row:true, time:time})
+            }else{
+                chat.messages.push({user_id:id, user_message:message, color:chat.color[id], row:false, time:time})
+            }
+        }else{
+            chat.messages.push({user_id:id, user_message:message, color:chat.color[id], row:false, time:time})
+        }
+        const user_color = chat.color[id]
+        const row = chat.messages[chat.messages.length - 1].row
+        socket.broadcast.emit('sendMessage', message, id, user_color, row, time)
+    }
+});
